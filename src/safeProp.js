c587175c4bbbd2475c3dc43d72beb7066339bb9c
Victor Buchalla <vs_buchalla@hotmail.com>
2018-11-30 15:43:32 -0200

    feat: exporting src in index file

--- src/safeProp.js
+++ src/safeProp.js
@@ -1,6 +1,6 @@
-const R = require('ramda');
-const { fromNullable } = require('folkate/maybe');
+const R = require('ramda')
+const { fromNullable } = require('folktale/maybe')
 
-const safeProp = R.curry(R.compose(fromNullable, R.prop));
+const safeProp = R.curry(R.compose(fromNullable, R.prop))
 
-module.exports = safeProp;
+module.exports = safeProp
