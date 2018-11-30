2c5e110da4b2afee9c5f70c681c563ebb122034c
Victor Buchalla <vs_buchalla@hotmail.com>
2018-11-30 15:41:10 -0200

    fix: removing not supported export

--- src/log.js
+++ src/log.js
@@ -1,6 +1,6 @@
-const R = require('ramda');
+const R = require('ramda')
 
-export const log = (prefix) =>
+const log = (prefix) =>
   R.tap((value) => console.log('%c%s: ', 'color: #C753B4; background: #E7FFE7; font-size: 24px;', prefix, value))
 
-module.exports = log;
+module.exports = log
