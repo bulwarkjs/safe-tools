fd2e01b7e4eb67555c314cc44b869d711c03ac77
Victor Buchalla <vs_buchalla@hotmail.com>
2018-11-30 12:16:32 -0200

    feat: removing folktale folder

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 module.exports = {
   removeNullables: require('./removeNullables'),
   isNilOrEmpty: require('./isNilOrEmpty'),
-  log: require('./log')
+  log: require('./log'),
+  fromAllNullables: require('./fromAllNullables'),
+  safeProp: require('./safeProp')
 }
