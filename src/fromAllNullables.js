5ac92114232a1fafba5c51206aded6b1f0b0d620
Victor Buchalla <vs_buchalla@hotmail.com>
2018-11-30 15:42:16 -0200

    style: fixing ident

--- src/fromAllNullables.js
+++ src/fromAllNullables.js
@@ -1,9 +1,9 @@
-const { Just, Nothing } = require('folktale/maybe');
+const { Just, Nothing } = require('folktale/maybe')
 
 const fromAllNullables = arr => {
   if(arr.every(item => item))
-    return Just(arr)
-   return Nothing()
+  { return Just(arr) }
+  return Nothing()
 }
 
-module.exports = fromAllNullables;
+module.exports = fromAllNullables
