9a50ae58f79a269f2061c091d54f3eba84352cd5
Victor Buchalla <vs_buchalla@hotmail.com>
2018-11-30 12:15:08 -0200

    feat: adding fromAllNullables

---
+++ src/folktale/fromAllNullables.js
@@ -0,0 +1,9 @@
+const { Just, Nothing } = require('folktale/maybe');
+
+const fromAllNullables = arr => {
+  if(arr.every(item => item))
+    return Just(arr)
+   return Nothing()
+}
+
+module.exports = fromAllNullables;
