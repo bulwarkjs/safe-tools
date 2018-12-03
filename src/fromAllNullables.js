9fc750a553dbd17b4ea0d063d1d55d6b7b9a1677
Victor Buchalla <vs_buchalla@hotmail.com>
2018-12-03 15:56:23 -0200

    fix: using isNil insted of true evaluation

--- src/fromAllNullables.js
+++ src/fromAllNullables.js
@@ -1,7 +1,8 @@
 const { Just, Nothing } = require('folktale/maybe')
+const R = require('ramda')
 
 const fromAllNullables = arr => {
-  if(arr.length && arr.every(item => item))
+  if(arr.length && arr.every(item => R.isNil(item)))
   { return Just(arr) }
   return Nothing()
 }
